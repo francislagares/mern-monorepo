5e7240fd3e3470b7e39806701454fd3190668018
Francis Lagares <francis.lagares@gmail.com>
2022-01-14 20:22:32 +0100

    chore: update dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
 
-const App = () => { 
+const App = (): JSX.Element => {
   return (
     <div className="App">
       <h1>Hello from React</h1>
     </div>
   );
-}
+};
 
 export default App;

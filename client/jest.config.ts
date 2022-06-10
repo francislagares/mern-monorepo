6ea85e132ed84a0ad830e5549e1730baa6f5d056
Francis Lagares <francis.lagares@gmail.com>
2022-06-10 21:14:54 +0200

    chore: update dependencies

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   displayName: 'client',
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
 };
 
 export default config;
